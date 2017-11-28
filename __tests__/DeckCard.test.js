5c376f1b0470f5a7ba0411446c7120aafea4b91c
brolam <breno@brolam.com.br>
2017-11-28 20:54:36 -0300

    test: TDD DeckCard - onSelect.

--- __tests__/DeckCard.test.js
+++ __tests__/DeckCard.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import DeckCard from '../components/DeckCard'
 
+const deckDummy = { id: 'id-one', title: 'One Deck', amountOfCards: 10 }
+
 test('renders without crashing', () => {
-  const deckDummy = { title: 'One Deck', amountOfCards: 10 }
   const wrapper = shallow(<DeckCard {...deckDummy} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('onSelect', () => {
+  let selectedDeckId = '-1'
+  const onSelect = (deckId) => { selectedDeckId = deckId }
+  const deckCard = shallow(<DeckCard {...deckDummy}  onSelect={onSelect} />);
+  deckCard.simulate('press')
+  expect(selectedDeckId).toBe(deckDummy.id)
 });
--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 import cardStyle from '../styles/cardStyles'
 
-export default function DeckCard({ title, amountOfCards }) {
+export default function DeckCard({
+  id,
+  title,
+  amountOfCards,
+  onSelect = (deckId) => { }
+  }) {
   return (
-    <View style={cardStyle}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.amountOfCards}>{amountOfCards}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+    >
+      <View style={cardStyle}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.amountOfCards}>{amountOfCards}</Text>
+      </View>
+    </TouchableOpacity >
   )
 }
 
 DeckCard.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   amountOfCards: PropTypes.number.isRequired,
 }
