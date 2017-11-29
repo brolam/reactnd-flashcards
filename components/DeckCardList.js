da3e297a0c8d8e3cde179d34b37bf54a53d8bcfc
brolam <breno@brolam.com.br>
2017-11-28 21:12:55 -0300

    refactor: DeckCardList - added onSelect DeckCard event.

--- components/DeckCardList.js
+++ components/DeckCardList.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import { StyleSheet, View, FlatList, TouchableOpacity } from 'react-native'
+import { View, FlatList, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 import DeckCard from './DeckCard'
 
 export default function DeckCardList({ decks, onSelectedOneDeck }) {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={decks}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => onSelectedOneDeck(item)}
-          >
-            <DeckCard
-              key={item.id} {...item}
-            />
-          </TouchableOpacity>
-        )}
-      />
-    </View>
+    <FlatList
+      data={decks}
+      renderItem={({ item }) => <DeckCard
+        key={item.id}
+        {...item}
+        onSelect={onSelectedOneDeck}
+      />}
+    />
   )
 }
 
 DeckCardList.propTypes = {
   decks: PropTypes.array.isRequired,
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
