93a4e57b929b788bd5a994a018891047196182e1
brolam <breno@brolam.com.br>
2017-12-03 07:48:42 -0300

    refactor: QuizCardStart new layout.

--- components/QuizCardStart.js
+++ components/QuizCardStart.js
@@ -3,15 +3,34 @@ import { StyleSheet } from 'react-native'
 import { View, Text, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 import cardStyle from '../styles/cardStyles'
+import buttonStyle from '../styles/buttonStyles'
+import { white, green, red, orange, lightPurp } from '../styles/colors'
 
 export default function QuizCardStart({ deck, quizzes, onStart = () => { } }) {
   return (
-    <View style={cardStyle}>
-      <Text style={styles.temp}>{deck.title}</Text>
-      <Text style={styles.temp}>{deck.amountOfCards} card(s)</Text>
-      <TouchableOpacity  id="buttonStart" onPress={onStart}>
-        <Text style={styles.temp} >Start</Text>
-      </TouchableOpacity>
+    <View style={[cardStyle, styles.container]}>
+      <Text style={styles.amountOfCards}>Last Score 99% </Text>
+      <Text style={styles.lastScore}>{deck.amountOfCards} cards</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={[buttonStyle, styles.buttonStart]}
+          id="buttonStart"
+          onPress={onStart}>
+          <Text style={styles.textButtons} >Start</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[buttonStyle, styles.buttonAdd]}
+          id="buttonAddQuiz"
+          onPress={() => console.log('Add quiz')}>
+          <Text style={styles.textButtons} >Add</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[buttonStyle, styles.buttonEdit]}
+          id="buttonEditQuiz"
+          onPress={() => console.log('Edit quiz')}>
+          <Text style={styles.textButtons} >Edit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -24,5 +43,47 @@ QuizCardStart.propTypes = {
 }
 
 const styles = StyleSheet.create({
-  temp: { fontSize: 24, textAlign: 'center', paddingTop: 8 },
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flex: 1,
+    marginBottom: 10
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    marginBottom: 5
+  },
+  buttonStart: {
+    backgroundColor: green,
+    margin: 5
+  },
+  buttonAdd: {
+    backgroundColor: orange,
+    margin: 5
+  },
+  buttonEdit: {
+    backgroundColor: lightPurp,
+    margin: 5
+  },
+  textButtons: {
+    color: white,
+    fontSize: 32,
+    fontWeight: '600',
+    padding: 5,
+  },
+  lastScore: {
+    fontSize: 42,
+    textAlign: 'center',
+    padding: 20,
+    fontWeight: '700',
+    marginBottom: 5,
+  },
+  amountOfCards: {
+    fontSize: 32,
+    textAlign: 'center',
+    fontWeight: '700',
+    marginBottom: 10,
+  },
 });
